9771cc225734374678d096ba21f2688f1852d3fb
Ruben <74729@protonmail.com>
2018-06-06 15:54:15 +0200

    Added a keyboard shortcut to quickly switch between Zendesk, Supportal2, and Supportal1

--- background.js
+++ background.js
@@ -56,6 +56,37 @@ chrome.runtime.onMessage.addListener(
 );
 
 
+// Open the next Supportal or Zendesk tab
+chrome.commands.onCommand.addListener(function(command) {
+    if (command == "goToNextPage") {
+        chrome.tabs.query({}, function(tabs) {
+            let tabIndex = -1;
+            let allTabs = []
+            // Find the last open Supportal tab
+            for (let i = 0; i < tabs.length; i++) {
+                if (tabs[i]["active"] === true) {
+                    tabIndex = tabs[i]["index"];
+                    break;
+                }
+            };
+            for (let j = tabIndex + 1; j < tabs.length; j++) {
+                allTabs.push(tabs[j])
+            }
+            for (let k = 0; k < tabIndex; k++) {
+                allTabs.push(tabs[k])
+            }
+            for (let k = 0; k < allTabs.length; k++) {
+                if (allTabs[k]["url"].startsWith("https://supportal2.blendle.io/") || allTabs[k]["url"].startsWith("https://blendle.zendesk.com/") || allTabs[k]["url"].startsWith("https://supportal.blendle.io/")) {
+                    chrome.tabs.update(allTabs[k]["id"], {"active": true});
+                    return;
+                };
+            }
+        });
+    };
+});
+
+
+
 // Copy the variable to the clipboard
 function copyToClipboard(text){
     var dummy = document.createElement("input");
