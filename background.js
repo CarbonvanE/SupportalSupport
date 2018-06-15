2708c90c62ec252bf2f3758cd8885b7433af8103
Ruben <74729@protonmail.com>
2018-06-15 18:43:45 +0200

    Added a content script for Zendesk and included a function that makes the comment box black with a white font

--- background.js
+++ background.js
@@ -75,9 +75,19 @@ chrome.runtime.onMessage.addListener(
 )
 
 
+// Check if the reply sent via Zendesk is a public reply or an internal note
+chrome.commands.onCommand.addListener(function(command) {
+    if (command === "checkIfComment") {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "send_alert"});
+        });
+    }
+});
+
+
 // Open the next Supportal or Zendesk tab
 chrome.commands.onCommand.addListener(function(command) {
-    if (command == "goToNextPage") {
+    if (command === "goToNextPage") {
         chrome.tabs.query({}, function(tabs) {
             let tabIndex = -1;
             let allTabs = [];
---
+++ zendeskCS.js
@@ -0,0 +1,25 @@
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    let textBox = document.querySelector('.fr-focus');
+    let isPublic = textBox.classList.contains('is-public');
+    if (isPublic === false) {
+        alert("This reply was sent as an internal note, NOT as a public reply!");
+    }
+    let buttons = document.querySelector('.ticket_submit_buttons');
+    let solvedButton = buttons.querySelector('.solved');
+});
+
+
+window.onload = function () {
+    setInterval(function() {
+        let textBox = document.querySelector('.fr-focus');
+        let isPublic = textBox.classList.contains('is-public');
+        let box = document.querySelector('.zendesk-editor--rich-text-comment');
+        if (isPublic === false) {
+            box.style.backgroundColor = "rgb(25, 25, 25)";
+            box.style.color = "white";
+        } else {
+            box.style.backgroundColor = "white";
+            box.style.color = "black";
+        }
+    }, 50)
+}
