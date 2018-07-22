3c2b0f4358b99bb99e70c8e5baba0f61523f1a67
Ruben <74729@protonmail.com>
2018-07-22 23:30:44 +0200

    refactor: Cleaned up zendeskCS.js

--- js/zendeskCS.js
+++ js/zendeskCS.js
@@ -2,25 +2,16 @@ window.onload = setInterval(function () {
     let all = document.querySelectorAll("a[href]");
     let elems = [];
     for (let index = 0; index < all.length; index++) {
-        // console.log(all[index]);
-        // console.log(all[index]["href"]);
         if (all[index]['href'].startsWith("mailto:") === true) {
-            elems.push([all[index]['href'].substring(7), all[index]]);
-            console.log([all[index]['href'].substring(7), all[index]])
+            let elem = all[index],
+                mail = elem['href'].substring(7);
+
+            elem.removeAttribute("href");
+            elem.setAttribute("style", "color: firebrick")
+            elem.classList.add("thisEmail");
+            elem.addEventListener('click', function() {
+                chrome.runtime.sendMessage({"loadSupportal": true, "mail": mail});
+            })
         }
     }
-    for (let index = 0; index < elems.length; index++) {
-        let elem = elems[index][1],
-            mail = elems[index][0];
-        elem.removeAttribute("href");
-        elem.setAttribute("style", "color: firebrick")
-        elem.classList.add("thisEmail");
-        // elem.onmouseover = function() {
-        //     elem.setAttribute("style", "font-weight: bold")
-        // }
-        elem.addEventListener('click', function() {
-            console.log(mail)
-            chrome.runtime.sendMessage({"loadSupportal": true, "mail": mail});
-        })
-    }
-},500);
+},1000);
