4aad4a73c5c953b3cd906599c535aa4847e1fdd5
Ruben <74729@protonmail.com>
2018-07-30 10:54:14 +0200

    feat: Temporarily removed a buggy implementation of the extension on the Zendesk website

--- js/zendeskCS.js
+++ js/zendeskCS.js
@@ -1,16 +1,16 @@
-window.onload = setInterval(function () {
-    let all = document.querySelectorAll("a[href]");
-    for (let index = 0; index < all.length; index++) {
-        if (all[index]['href'].startsWith("mailto:") === true) {
-            let elem = all[index],
-                mail = elem['href'].substring(7);
-
-            elem.removeAttribute("href");
-            elem.setAttribute("style", "color: firebrick")
-            elem.classList.add("thisEmail");
-            elem.addEventListener('click', function() {
-                chrome.runtime.sendMessage({"contents": "Load Supportal", "mail": mail});
-            })
-        }
-    }
-},1000);
+// window.onload = setInterval(function () {
+//     let all = document.querySelectorAll("a[href]");
+//     for (let index = 0; index < all.length; index++) {
+//         if (all[index]['href'].startsWith("mailto:") === true) {
+//             let elem = all[index],
+//                 mail = elem['href'].substring(7);
+//
+//             elem.removeAttribute("href");
+//             elem.setAttribute("style", "color: firebrick")
+//             elem.classList.add("thisEmail");
+//             elem.addEventListener('click', function() {
+//                 chrome.runtime.sendMessage({"contents": "Load Supportal", "mail": mail});
+//             })
+//         }
+//     }
+// },1000);
